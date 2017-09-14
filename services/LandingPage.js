f58a9dbf0e5d2c6b0bbedc7ed448fc914d244ef2
Fred Chien <cfsghost@gmail.com>
2017-09-14 12:52:59 +0800

    upgrade engined framework

--- services/LandingPage.js
+++ services/LandingPage.js
@@ -1,18 +1,17 @@
 const config = require('config');
-const Router = require('koa-router')
 const { RouterService } = require('engined-http');
-
-// Create a new router
-const router = Router();
-
-router.get('/', async (ctx, next) => {
-
-	ctx.body = 'Hello! ' + config.appName;
-});
 
 module.exports = class extends RouterService() {
 
 	async setupRoutes() {
+
+		const router = this.createRouter();
+
+		router.get('/', async (ctx, next) => {
+
+			ctx.body = 'Hello! ' + config.appName;
+		});
+
 		return router;
 	}
 };
